b8cb9aeaf6749525b5b26d56121bc9346f7e90eb
Sarath K <>
2026-08-27 14:58:27 +0530

    eposide1

---
+++ App.js
@@ -0,0 +1,7 @@
+
+const heading = React.createElement("div", 
+    {id:"parent"}, 
+ [React.createElement("div", {id:"child"}, [React.createElement("h1", {}, "I'm an H1 tag"),React.createElement("h2", {}, "I'm an H2 tag")]),React.createElement("div", {id:"child2"}, [React.createElement("h1", {}, "I'm an H3 tag"),React.createElement("h2", {}, "I'm an H4 tag")])]
+)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
